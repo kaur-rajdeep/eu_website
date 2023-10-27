68fe037471268137349f844926ad956018fe2b71
NegiShweta <shwetanegi0066@gmail.com>
2023-10-27 11:04:24 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import Student from './pages/Student'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Student/>
   </React.StrictMode>
 )
