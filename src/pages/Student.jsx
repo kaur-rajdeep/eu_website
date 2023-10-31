d4c72aec0dfe219e05b8dc1965756b35abfb9cd1
NegiShweta <shwetanegi0066@gmail.com>
2023-10-31 11:45:58 +0530

    commit

--- src/pages/Student.jsx
+++ src/pages/Student.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
-
+import React from 'react';
+import ImageSlider from "../components/Carousel"
 export default function Student() {
-  return (
-    <div className="mt-16">Student Zone : Will be made by shweta negi</div>
-  );
+  const slides = [
+    { url: "https://th.bing.com/th/id/OIP.6af_KSwpOoj7pJWKX0yhIAHaFj?w=225&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7", title: "image1" },
+    { url: "https://th.bing.com/th/id/OIP.LS0c-TEMqb5A4vI1MUTPlQHaDg?w=322&h=165&c=7&r=0&o=5&dpr=1.3&pid=1.7", title: 'image2' },
+    { url: "https://th.bing.com/th/id/OIP.yY-E-Mu1SHC02rwOHKC1XgHaDm?w=327&h=170&c=7&r=0&o=5&dpr=1.3&pid=1.7", title: "image3" },
+    { url: "https://th.bing.com/th/id/OIP.V5VDHg7SwqbTACc3VjpYswHaFi?w=204&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7", title: 'image4' },
+];
+    const containerStyles = {
+        width: "500px",
+        height: "280px",
+        margin: "0 auto",
+      };
+    return (
+        <div className="mt-16">Student Zone: Will be made by Shweta Negi
+        <div style={containerStyles}>
+        <ImageSlider slides={slides} />
+      </div>
+        
+        </div>
+    );
 }
