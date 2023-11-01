92dce1f638a21f27efe6a142d6ab5f7fc4d48620
NegiShweta <shwetanegi0066@gmail.com>
2023-11-01 12:24:49 +0530

    resolving issues

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,17 +1,9 @@
-// /*
-// import React from 'react';
-// import { Route } from 'react-router-dom';
-// import Contact from './Contact'; // Update the import path to match your directory structure
+import React from 'react'
 
-// function App() {
-//   return (
-//     <div>
-//       {/* Other routes */}
-//       <Route path="/contact" component={Contact} />
-//       {/* Other routes */}
-//     </div>
-//   );
-// }
+function About() {
+  return (
+    <div>About</div>
+  )
+}
 
-// export default App;
-
+export default About
