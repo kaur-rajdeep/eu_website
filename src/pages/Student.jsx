98457df7170209cb7536780140910300eff46ae7
kaur-rajdeep <kaurd3535@gmail.com>
2023-11-01 11:43:21 +0530

    fixing deployment error

--- src/pages/Student.jsx
+++ src/pages/Student.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Carousel from '../components/Carousel';
+// import Carousel from '../components/Carousel';
 export default function Student() {
   const images = [
     { url: "https://th.bing.com/th/id/OIP.6af_KSwpOoj7pJWKX0yhIAHaFj?w=225&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7", title: "image1" },
