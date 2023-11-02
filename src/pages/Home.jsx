3b53fef0875c6447d2a48ee22395d99a1d710768
kaur-rajdeep <kaurd3535@gmail.com>
2023-11-02 11:01:01 +0530

    added notice board component on Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 import Carousel from "../components/Carousel";
 import GetInTouch from "../components/GetInTouch";
+import NoticeBoard from "../components/NoticeBoard";
 
 function Home() {
+  const images = [
+    "https://eternaluniversity.edu.in/images/activity/AGY01657703203EducationalVisit.jpg",
+    "https://eternaluniversity.edu.in/images/activity/AGY11657703203EducationalVisit.jpg",
+    "https://eternaluniversity.edu.in/images/event/MI1656487146Conference.jpg",
+    "https://eternaluniversity.edu.in/images/activity/AMI1660805268CollegeActivity.jpg",
+    "https://eternaluniversity.edu.in/images/event/MI1660799332Camp.jpg",
+  ];
+
+  const containerStyles = {
+    width: "500px",
+    height: "280px",
+    margin: "0 ",
+  };
+
   return (
     <>
-      {/* // IMAGES CAROUSEL
-      // MISSION */}
+      {/* IMAGES CAROUSEL */}
+      <Carousel images={images} />
+      {/* MISSION */}
       <div className="color mt-16 h-[50vh] grid justify-center">
-        <p className="font-bold lg:text-6xl text-4xl text-center text-white lg:mt-12 mt-16">
+        <p className="font-extrabold lg:text-6xl text-4xl text-center text-white lg:mt-12 mt-16">
           OUR MISSION
         </p>
         <p className="text-white lg:text-2xl md:w-[70vw] text-center lg:-mt-16">
@@ -18,10 +34,11 @@ function Home() {
           mankind with compassion and love."
         </p>
       </div>
-      {/* // WHY ETERNAL UNIVERSITY
-      // COLLEGES
-      // NOTICE BOARD
-      // GET IN TOUCH */}
+      {/* // WHY ETERNAL UNIVERSITY */}
+      {/* // COLLEGES */}
+      {/* // NOTICE BOARD  */}
+      <NoticeBoard />
+      {/* // GET IN TOUCH */}
       <GetInTouch />
     </>
   );
