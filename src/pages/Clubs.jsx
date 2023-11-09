41ddf15deb0be00ba2cdd976c308b52497496776
NegiShweta <shwetanegi0066@gmail.com>
2023-11-09 21:35:26 +0530

    Dleleting the Image Card SLider component and adding Notice Board component

--- src/pages/Clubs.jsx
+++ src/pages/Clubs.jsx
@@ -1,24 +1,17 @@
 import React, { useState } from "react";
 import Carousel from "../components/Carousel";
 import RegistrationForm from "../components/RegistrationForm";
-import SlidingCards from "../components/ImageSlidingCards";
-import image1 from '../assets/img1.jpeg';
-import image2 from '../assets/img2.jpeg';
-import image3 from '../assets/img3.jpeg';
-import image4 from '../assets/img4.jpeg';
+import ClubsData from "../store/Clubs_Image_Card_Data";
+import SlidingCards from "../components/NoticeBoard";
 
 const images = [
-  "https://eternaluniversity.edu.in/images/activity/AGY01657703203EducationalVisit.jpg",
-  "https://eternaluniversity.edu.in/images/activity/AGY11657703203EducationalVisit.jpg",
-  "https://eternaluniversity.edu.in/images/event/MI1656487146Conference.jpg",
-  "https://eternaluniversity.edu.in/images/activity/AMI1660805268CollegeActivity.jpg",
-  "https://eternaluniversity.edu.in/images/event/MI1660799332Camp.jpg",
-];
-
-
-
-const ClubImages = [image1, image2, image3,image4];
-
+    "https://eternaluniversity.edu.in/images/activity/AGY01657703203EducationalVisit.jpg",
+    "https://eternaluniversity.edu.in/images/activity/AGY11657703203EducationalVisit.jpg",
+    "https://eternaluniversity.edu.in/images/event/MI1656487146Conference.jpg",
+    "https://eternaluniversity.edu.in/images/activity/AMI1660805268CollegeActivity.jpg",
+    "https://eternaluniversity.edu.in/images/event/MI1660799332Camp.jpg",
+  ];
+  
 function Clubs() {
   const [showRegistrationForm, setShowRegistrationForm] = useState(false);
   const [isSubmitted, setIsSubmitted] = useState(false);
@@ -38,22 +31,20 @@ function Clubs() {
       <Carousel images={images} />
       
       <div className="h-[50vh] grid justify-center bg-[#072e33] text-white">
-      <p className="lg:text-5xl md:text-4xl text-3xl text-center lg:mt-12 mt-16 font-[990]">
-        Clubs
+        <p className="lg:text-5xl md:text-4xl text-3xl text-center lg:mt-12 mt-16 font-[990]">
+          Clubs
         </p>
         <p className="lg:text-2xl md:w-[70vw] text-center lg:-mt-12 -mt-16 md:text-xl">
           "Joining a college club is like unlocking a secret level—except instead of battling monsters, you conquer new friendships and navigate the maze of exciting events. It's the ultimate cheat code for a memorable college experience!"
         </p>
       </div>
       
-      
-      <SlidingCards images={ClubImages} />
-    
+      <SlidingCards images={ClubsData} />
     
       {!showRegistrationForm || isSubmitted ? (
-        <div className="text-center mt-14 mb-10">
+        <div className="text-center mt-10 mb-10">
           <button
-            className="bg-[#072e33] text-white btn btn-primary w-60 h-14 rounded-full text-xl font-bold  "
+            className="bg-[#072e33] text-white btn btn-primary w-60 h-14 rounded-full text-xl font-bold "
             onClick={handleRegistrationFormClick}>
             Registration Form
           </button>
---
+++ src/store/Clubs_Image_Card_Data.js
@@ -0,0 +1,35 @@
+import image1 from '../assets/img1.jpeg'
+import image2 from '../assets/img2.jpeg';
+import image3 from '../assets/img3.jpeg';
+import image4 from '../assets/img4.jpeg';
+
+const ClubsData = [
+    {
+      id: 1,
+      imageUrl: image1,
+      name: "College Name 1",
+      description: "Description for College 1.",
+    },
+    {
+      id: 2,
+      imageUrl: image2,
+      name: "College Name 2",
+      description: "Description for College 2.",
+    },
+    {
+      id: 3,
+      imageUrl: image3,
+      name: "College Name 3",
+      description: "Description for College 3.",
+    },
+    {
+      id: 4,  // Changed to a unique identifier
+      imageUrl: image4,
+      name: "College Name 4",
+      description: "Description for College 4.",
+    },
+    // Add more objects as needed
+  ];
+  
+  export default ClubsData;
+  
